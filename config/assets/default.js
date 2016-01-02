bdd6e6e3f127ac431e4c4152fd96e1b6be2927d4
root <root@Helei.io>
2016-01-02 02:40:16 -0500

    first real update

--- config/assets/default.js
+++ config/assets/default.js
@@ -15,7 +15,8 @@ module.exports = {
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-        'public/lib/angular-file-upload/angular-file-upload.js'
+        'public/lib/angular-file-upload/angular-file-upload.js',
+          'public/lib/ngmap/build/scripts/ng-map.min.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
